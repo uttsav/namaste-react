99cd6c431d83e8a0f5d1ab87a0106746c7dd70c8
Utsav Oza <UO00933812@techmahindra.com>
2023-03-27 16:25:35 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+const parent = React.createElement('div',{id:'parent'},
+                [React.createElement('div',{id:'child'},[
+                    React.createElement('h1',{id:'head',key:'h1'},'I am h1 Tag'),
+                    React.createElement('h2',{id:'head',key:'h2'},'I am h2 Tag')]
+                ),React.createElement('div',{id:'child2'},[
+                    React.createElement('h1',{id:'head',key:'h1'},'I am h1 Tag'),
+                    React.createElement('h2',{id:'head',key:'h2'},'I am h2 Tag')]
+                )
+                ]);
+
+
+const heading = React.createElement("h1",{id:'heading'},"Hello World from External React!!");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(parent);
