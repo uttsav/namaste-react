313decf1aabf21394bc656ec0abc9b97ac82ff0a
Utsav Oza <UO00933812@techmahindra.com>
2023-03-29 10:14:58 +0530

    adding package and package lock

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-Namaste React 
+#Namaste React 
+
+
+# parcel
+- Dev Build
+- Local server
+- HMR = Hot Module Replacement
+- Caching the changes
+- File watching algo
+- Image optimization
+- Minification of files
+- Bundling of files
+- Compressing of files
+- Consistent hashing
+- Code spliting
+- Differential Bundling : app runs on older browsers easily
+- Diagnostics
+- Error Handling
+- can Host on HTTPs
+- Tree shaking Algo -  remove unused code for you
+- Different dev and prod bundle
--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-
+import React from "react";
+import ReactDOM  from "react-dom/client";
 
 const parent = React.createElement('div',{id:'parent'},
                 [React.createElement('div',{id:'child'},[
-                    React.createElement('h1',{id:'head',key:'h1'},'I am h1 Tag'),
-                    React.createElement('h2',{id:'head',key:'h2'},'I am h2 Tag')]
+                    React.createElement('h1',{id:'head',key:'h1'},'This is Namaste react'),
+                    React.createElement('h2',{id:'head',key:'h2'},'By Utsav Oza')]
                 ),React.createElement('div',{id:'child2'},[
                     React.createElement('h1',{id:'head',key:'h1'},'I am h1 Tag'),
                     React.createElement('h2',{id:'head',key:'h2'},'I am h2 Tag')]
